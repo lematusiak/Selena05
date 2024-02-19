94861aa1fbb54fd09858fe5a05bea14e7d81e2c3
lematusiak <leematusiak@gmail.com>
2024-02-19 18:56:21 +0100

    Moduł 5 (Lista + Pętla Foreach 19.02.24)

--- Program.cs
+++ Program.cs
@@ -1,47 +1,48 @@
-﻿// TABLICE
-
-using System.Security.Cryptography;
-
-// PIERWSZY SPOSÓB
+﻿// MODUŁ 5 (LISTA Z PĘTLĄ FOREACH) 19.02.2024
 
 int[] grades = new int[365];
-string[] dayOfWeek = new string[7];
-dayOfWeek[0] = "monday";
-dayOfWeek[1] = "tuesday";
-dayOfWeek[2] = "wednesday";
-dayOfWeek[3] = "thursday";
-dayOfWeek[4] = "friday";
-dayOfWeek[5] = "saturday";
-dayOfWeek[6] = "sunday";
+List<string>dayOfWeek = new List<string>(7);
 
-// Console.WriteLine(dayOfWeek[3]);
+dayOfWeek.Add("monday");
+dayOfWeek.Add("tuesday");
+dayOfWeek.Add("wednesday");
+dayOfWeek.Add("thursday");
+dayOfWeek.Add("friday");
+dayOfWeek.Add("saturday");
+dayOfWeek.Add("sunday");
 
-// DRUGI SPOSÓB
+// zwróci wszystkie dni tygodnia (PĘTLA FOR)
 
-// string[] dayOfWeek2 = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
-
-// Console.WriteLine(dayOfWeek2[0]);
-
-// i++ to to samo, co i = i + 1
-// to pętla w najczystszej postaci, wyświetlą się wszystkie dni tygodnia
-
-for (var i = 0; i < 7; i++)
+for (var i = 0; i < dayOfWeek.Count; i++)
 {
     Console.WriteLine(dayOfWeek[i]);
 }
 
-// a w tej pętli zabezpieczamy się przed tym, gdyby ktoś w kodzie kiedys
-// wartość 7 na inną
+// podobnie ta pętla również zwróci dni tygodnia (PĘTLA FOREACH)
+// chodzi o to, że zrobi to dużo prościej
 
-for (var i = 0; i < dayOfWeek.Length; i++)
+foreach (var day in dayOfWeek)
 {
-    Console.WriteLine(dayOfWeek[i]);
-}
-
-// a w ostatniej pętli zwrócony zostanie monday, thursday, sunday (co trzeci dzień)
-for (var i = 0; i < dayOfWeek.Length; i= i+3)
-{
-    Console.WriteLine(dayOfWeek[i]);
-}
+    Console.WriteLine(day); 
+} 
 
 Console.ReadKey();
+
+// Pętla "for" jest bardziej ogólnym rodzajem pętli, która wykonuje blok kodu wielokrotnie na podstawie określonego warunku.
+// Możemy zdefiniować początkową wartość liczby iteracji, warunek zatrzymania oraz zwiększanie wartości iteracji wewnątrz pętli.
+// Pętla "for" jest przydatna, gdy znamy dokładną liczbę iteracji, jaką chcemy wykonać.
+
+// Z drugiej strony pętla "foreach" jest używana do iterowania po elementach
+// kolekcji, takich jak tablice, listy czy słowniki. Nie musimy określać wartości
+// iteracji, warunków zatrzymania czy zwiększania wartości iteracji,
+// ponieważ pętla "foreach" automatycznie przechodzi przez
+// wszystkie elementy kolekcji.
+// Jest to bardziej wygodne i czytelne rozwiązanie,
+// gdy chcemy przetworzyć wszystkie elementy kolekcji.
+
+// Przykładem z życia codziennego może być lista zakupów:
+// używając pętli "for" można by przejść przez każdy element listy,
+// jednocześnie zliczając ilość zakupionych przedmiotów.
+// Pętla "foreach" byłaby przydatna, gdybyśmy chcieli wypisać wszystkie
+// zakupione przedmioty bez konieczności zliczania ich ilości.
+
